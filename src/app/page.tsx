b095c774948818a7e1f33f6d6fac518f0717165f
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-05-08 16:12:58 -0300

    add:scroll to top button on page end. fix dots on carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,8 @@ import services from '@/public/home/newArrival/botton/services.svg'
 import services2 from '@/public/home/newArrival/botton/services2.svg'
 import services3 from '@/public/home/newArrival/botton/services3.svg'
 
+import { HiArrowUp } from "react-icons/hi";
+
 export default function Home() {
   return (
     <main className="w-11/12 lg:w-5/6 mx-auto ">
@@ -94,23 +96,28 @@ export default function Home() {
           <NewArrival />
         </div>
       </section>
-      <section className="flex flex-col justify-center gap-20 items-center mb-36 lg:flex-row lg:gap-8 xl:gap-[5.5rem]">
+      <section className="flex flex-col justify-center gap-20 items-center mb-16 lg:flex-row lg:gap-8 xl:gap-[5.5rem]">
         <div className="flex flex-col items-center">
-          <Image src={services} width={100} height={0} alt="" className="mb-6"/>
+          <Image src={services} width={100} height={0} alt="" className="mb-6" />
           <span className="mb-2 font-semibold text-xl">FREE AND FAST DELIVERY</span>
           <span className="text-sm">Free delivery for all orders over $140</span>
         </div>
         <div className="flex flex-col items-center">
-          <Image src={services2} width={100} height={0} alt="" className="mb-6"/>
+          <Image src={services2} width={100} height={0} alt="" className="mb-6" />
           <span className="mb-2 font-semibold text-xl">24/7 CUSTOMER SERVICE</span>
           <span className="text-sm">Friendly 24/7 customer support</span>
         </div>
         <div className="flex flex-col items-center">
-          <Image src={services3} width={100} height={0} alt="" className="mb-6"/>
+          <Image src={services3} width={100} height={0} alt="" className="mb-6" />
           <span className="mb-2 font-semibold text-xl">MONEY BACK GUARANTEE</span>
           <span className="text-sm">We reurn money within 30 days</span>
         </div>
       </section>
+      <div className="flex justify-end mb-8">
+        <Link href="#" className="bg-[#ecebeb] p-3 rounded-full w-12 h-12">
+          <HiArrowUp size="auto" />
+        </Link>
+      </div>
     </main>
   )
 }
